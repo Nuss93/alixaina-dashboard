d2f6b5b66df1611de2ecde87f14725a5e19debc2
Nuss93 <nasreenrzk@gmail.com>
2023-08-09 22:01:11 +0800

    change path

--- src/config.js
+++ src/config.js
@@ -3,13 +3,13 @@
 // 0 = development build
 
 const CONFIG = 0;
-const options = {
-    path : 'ainaxali',
-    title : 'Aina X Ali'
-}
 // const options = {
-//     path : 'alixaina',
-//     title : 'ALI punya side'
+//     path : 'ainaxali',
+//     title : 'Aina X Ali'
 // }
+const options = {
+    path : 'alixaina',
+    title : 'ALI punya side'
+}
 
 export {CONFIG, options}
